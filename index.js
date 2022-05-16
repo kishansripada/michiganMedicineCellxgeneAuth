8a8bc07d219a4def244f54e76c81823e29bec947
kishansripada <kishansripada@gmail.com>
2022-05-15 20:35:27 -0400

    even better

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 var path = require('path');
-const cookieParser = require('cookie-parser');
 app.use(express.static("public"));
 
-app.use(cookieParser());
 
 
 app.get('/', (req, res) => {

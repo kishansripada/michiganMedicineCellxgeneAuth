1ba0c532d368ed7906145b0a2b7d8b53be4b1878
kishansripada <kishansripada@gmail.com>
2022-06-15 09:31:42 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -11,21 +11,33 @@ function setCookie(cname, cvalue, exhours) {
 btn.onclick = async function () {
     let password = document.getElementById("password").value
     let datasetUrl = await fetch(`${window.origin}/login/${password}`).then(r => r.json())
+    console.log(datasetUrl.DATASET)
     if (datasetUrl) {
         setCookie("auth", "XXVeh5U9hVu7Q87rhKQ51g==", 1)
 
         // change config vars in heroku
-        fetch("https://api.heroku.com/apps/michigan-cellxgene/config-vars", {
-            "method": "PATCH",
-            "headers": {
-                "Content-Type": "application/json",
-                "Accept": "application/vnd.heroku+json; version=3",
-                "Authorization": "Bearer 7143941a-cb61-4943-837a-fb0085cfb660"
+        // fetch("https://api.heroku.com/apps/michigan-cellxgene/config-vars", {
+        //     "method": "PATCH",
+        //     "headers": {
+        //         "Content-Type": "application/json",
+        //         "Accept": "application/vnd.heroku+json; version=3",
+        //         "Authorization": "Bearer 7143941a-cb61-4943-837a-fb0085cfb660"
+        //     },
+        //     "body": {
+        //         "DATASET": datasetUrl.DATASET
+        //     }
+        // })
+        const options = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/vnd.heroku+json; version=3',
+                Authorization: 'Bearer 7143941a-cb61-4943-837a-fb0085cfb660'
             },
-            "body": {
-                "DATASET": datasetUrl.DATASET
-            }
-        }).then(r => {
+            body: `{"DATASET":"${datasetUrl.DATASET}"}`
+        };
+
+        fetch('https://api.heroku.com/apps/michigan-cellxgene/config-vars', options).then(r => {
             // console.log(`loading dataset with url ${r.DATASET}`)
             window.location.href = `${window.origin}/app`
         }).catch(err => {
